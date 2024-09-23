46a0601481763cbc9c3c55f846f7e502df67567b
IluhaSich <130484685+IluhaSich@users.noreply.github.com>
2024-09-23 23:58:39 +0300

    basic functional wirh mongoDB

---
+++ rosLearning/src/main/java/com/ytrewq/rosLearning/entity/mongo/Task.java
@@ -0,0 +1,25 @@
+package com.ytrewq.rosLearning.entity.mongo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "tasks")
+public class Task {
+    @Id
+    private String id;
+
+    public Task() {
+    }
+
+    public Task(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
