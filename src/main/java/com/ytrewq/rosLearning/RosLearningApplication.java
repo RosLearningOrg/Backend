e0e86bcb88a62a10f85f3dbd8468ae51d61c508e
rogoz <rogozhin-04@inbox.ru>
2024-10-19 00:10:25 +0300

    asdasd

--- src/main/java/com/ytrewq/rosLearning/RosLearningApplication.java
+++ src/main/java/com/ytrewq/rosLearning/RosLearningApplication.java
@@ -2,6 +2,9 @@ package com.ytrewq.rosLearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RosLearningApplication {
@@ -10,4 +13,21 @@ public class RosLearningApplication {
 		SpringApplication.run(RosLearningApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:63342")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+//				registry.addMapping("/api/csrf").allowedOrigins("http://localhost:63342");
+//				registry.addMapping("/api/login").allowedOrigins("http://localhost:63342").allowedMethods("GET", "POST", "PUT", "DELETE")
+				;
+
+			}
+		};
+	}
 }
